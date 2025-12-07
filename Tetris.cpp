4e8db03a42da457f48dbb0bc6d94685d42e1fab5
ADMIN <ndaik0000@gmail.com>
2025-12-07 10:39:41 +0700

    #3 - Mỹ Duyên viết lại giao diện chỗ viền và block

--- Tetris.cpp
+++ Tetris.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <conio.h>
 #include <windows.h>
+#include <ctime> // for time()
+
 using namespace std;
 #define H 20
 #define W 15
@@ -10,42 +12,6 @@ char blocks[][4][4] = {
          {' ','I',' ',' '},
          {' ','I',' ',' '},
          {' ','I',' ',' '}},
-        {{' ','I',' ',' '},
-         {' ','I',' ',' '},
-         {' ','I',' ',' '},
-         {' ','I',' ',' '}},
-        {{' ',' ',' ',' '},
-         {' ','O','O',' '},
-         {' ','O','O',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {' ','O','O',' '},
-         {' ','O','O',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {' ','O','O',' '},
-         {' ','O','O',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {' ','O','O',' '},
-         {' ','O','O',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {' ','O','O',' '},
-         {' ','O','O',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {' ','O','O',' '},
-         {' ','O','O',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {' ','O','O',' '},
-         {' ','O','O',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {'I','I','I','I'},
-         {' ',' ',' ',' '},
-         {' ',' ',' ',' '}},
         {{' ',' ',' ',' '},
          {' ','O','O',' '},
          {' ','O','O',' '},
@@ -72,9 +38,63 @@ char blocks[][4][4] = {
          {' ',' ',' ',' '}}
 };
 
-int x=4,y=0,b=1;
+// --------------
+// --- Đồ họa ---
+// --------------
+
+// Màu sắc
+enum Color { 
+    red = 12, 
+    green = 10,
+    blue = 9, 
+    yellow = 14, 
+    cyan = 11, 
+    white = 15,
+    purple = 13,
+    orange = 6,
+    gray = 8
+    // Xem thêm tại bảng màu "Windows Console Color Table"
+};
+
+// Đổi màu chữ cho console Windows
+void setColor(int color) {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+// Chuyển thành block màu
+char ColorBlock(char c, int color) {
+    setColor(color);
+    return '\xDB';
+}
+
+char blockChar(char c) {
+    switch(c) {
+        case 'I': 
+            return ColorBlock(c, cyan);
+        case 'O': 
+            return ColorBlock(c, yellow);
+        case 'T': 
+            return ColorBlock(c, purple);
+        case 'S': 
+            return ColorBlock(c, green);
+        case 'Z':
+            return ColorBlock(c, red);
+        case 'J': 
+            return ColorBlock(c, blue);
+        case 'L': 
+            return ColorBlock(c, orange);
+        case '#':
+            return ColorBlock(c, gray);
+        default: 
+            return ' ';
+    }
+}
+//---------------
+
+int x=4,y=0,b=1; // b is the index of the current block
+
 void gotoxy(int x, int y) {
-    COORD c = {x, y};
+    COORD c = {(SHORT)x, (SHORT)y};
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 }
 void boardDelBlock(){
@@ -95,12 +115,18 @@ void initBoard(){
             if ((i==H-1) || (j==0) || (j == W-1)) board[i][j] = '#';
             else board[i][j] = ' ';
 }
+
 void draw(){
     gotoxy(0,0);
     for (int i = 0 ; i < H ; i++, cout<<endl)
         for (int j = 0 ; j < W ; j++)
-            cout<<board[i][j];
+        {
+            // cout<< board[i][j] << board[i][j];
+            cout << blockChar(board[i][j]) << blockChar(board[i][j]);
+            setColor(white);
+        }
 }
+
 bool canMove(int dx, int dy){
     for (int i = 0 ; i < 4 ; i++)
         for (int j = 0 ; j < 4 ; j++)
@@ -116,6 +142,7 @@ void removeLine(){
    
 }
 
+
 int main()
 {
     srand(time(0));
@@ -124,13 +151,17 @@ int main()
     initBoard();
     while (1){
         boardDelBlock();
-        if (kbhit()){
-            char c = getch();
-            if (c=='a' && canMove(-1,0)) x--;
-            if (c=='d' && canMove(1,0) ) x++;
-            if (c=='x' && canMove(0,1))  y++;
-            if (c=='q') break;
+        if (kbhit()){ // kbhit() checks if a key is pressed
+            int c = getch();
+            if (c == 0 || c == 224) {
+                c = getch(); 
+            }
+            if (c==75 && canMove(-1,0)) x--;    // Left arrow key
+            if (c==77 && canMove(1,0)) x++;     // Right arrow
+            if (c==80 && canMove(0,1))  y++;    // Down arrow
+            // if (c==72) rotate_block();                   // Up arrow
         }
+
         if (canMove(0,1)) y++;
         else {
             block2Board();
